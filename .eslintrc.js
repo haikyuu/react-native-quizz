f84b4ae9c7ee5de7979478f49cf309ee662ae0db
alaoui abdellah <als.abdellah@gmail.com>
2020-08-10 17:01:53 +0100

    ci: :sparkles: Add prettier, eslint, jest and setup CI CD (#1)
    * Add prettier, eslint, jest and setup CI CD
    
    Tests: passes CI with no tests to allow for initial passing.
    Prettier is configured with the defaults.
    Eslint is configured to work well with Prettier.
    CI checks for typescript errors, linting, prettier styles and then publishes to a specific channel for each PR.
    
    * add precommit hook using lint staged to lint and prettify
    
    * add staging and production workflows to publish on merge to master or develop

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
--- App.tsx
+++ App.tsx
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+type AppProps = Record<string, unknown>;
+
+const App: React.FunctionComponent<AppProps> = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
